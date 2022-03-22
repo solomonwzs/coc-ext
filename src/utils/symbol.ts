6c93d19a95a59dcfb7a0aea8d8ae1b1a30e37486
solomon <solomon.wzs@gmail.com>
2022-03-23 00:26:28 +0800

    modify 	modified:   src/utils/symbol.ts

--- src/utils/symbol.ts
+++ src/utils/symbol.ts
@@ -1,21 +1,32 @@
-import { languages, workspace, CancellationTokenSource } from 'coc.nvim';
+import {
+  languages,
+  workspace,
+  CancellationTokenSource,
+  DocumentSymbol,
+} from 'coc.nvim';
 import { logger } from './logger';
 
-export async function getDocumentSymbols(bufnr: number): Promise<any> {
+export async function getDocumentSymbols(
+  bufnr0?: number
+): Promise<DocumentSymbol[] | null> {
+  const { nvim } = workspace;
+  const bufnr = bufnr0 ? bufnr0 : ((await nvim.call('bufnr', ['%'])) as number);
   const doc = workspace.getDocument(bufnr);
   if (!doc || !doc.attached) {
-    return;
+    return null;
   }
 
   // @ts-ignore
   if (!languages.hasProvider('documentSymbol', doc.textDocument)) {
-    return;
+    return null;
   }
 
+  const tokenSource = new CancellationTokenSource();
+  const { token } = tokenSource;
   // @ts-ignore
-  const x = await languages.getDocumentSymbol(
+  const docSymList = (await languages.getDocumentSymbol(
     doc.textDocument,
-    new CancellationTokenSource()
-  );
-  logger.debug(x);
+    token
+  )) as DocumentSymbol[];
+  return docSymList;
 }
